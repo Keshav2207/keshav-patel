46085c91a1679c657eb83635d44548fae7e88ba3
Keshav Patel <70221686+Keshav2207@users.noreply.github.com>
2026-01-20 19:57:36 +0000

    Frontend fixes

--- javascripts/resumecards.js
+++ javascripts/resumecards.js
@@ -2,17 +2,24 @@ $(document).ready(function () {
 
   var $body = $('body');
   var $themeMenu = $('.resume-dropdown-menu > li > a');
+  var $themeBtn = $('#theme-toggle-btn');
 
-  $themeMenu.on('click', function(event) {
+  $themeMenu.on('click', function (event) {
 
-    $body.attr('class', '');
+    $body.removeClass(function (i, c) {
+      return (c.match(/(^|\s)theme-\S+/g) || []).join(' ');
+    });
 
     var themeColor = $(this).attr('id');
     var themeColorClass = "theme-" + themeColor;
     $body.addClass(themeColorClass);
 
-    event.preventDefault();
+    $themeBtn.html(
+      themeColor.charAt(0).toUpperCase() + themeColor.slice(1) +
+      ' <span class="caret"></span>'
+    );
 
+    event.preventDefault();
   });
 
 });
