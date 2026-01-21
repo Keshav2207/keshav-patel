c40f0b68c7afd8daccbb65a9597a5b0c2050ea7f
Keshav2207 <patelkeshav116@gmail.com>
2026-01-22 01:25:10 +0530

    Added carousel

--- javascripts/resumecards.js
+++ javascripts/resumecards.js
@@ -23,3 +23,56 @@ $(document).ready(function () {
   });
 
 });
+
+$(function () {
+  var $track = $('.resume-track');
+  var $slides = $track.children('.resume-slide');
+
+  var visible = 3;
+  var slideCount = $slides.length;
+  if (slideCount <= visible) return;
+
+  var index = visible;
+  var autoDelay = 3000;
+  var timer;
+  var slideWidth = 100 / visible;
+
+  /* Clone slides for infinite loop */
+  var $head = $slides.slice(0, visible).clone();
+  var $tail = $slides.slice(-visible).clone();
+  $track.prepend($tail).append($head);
+
+  var totalSlides = $track.children().length;
+
+  function setPos(noAnim) {
+    $track.css('transition', noAnim ? 'none' : 'transform 0.5s ease');
+    $track.css(
+      'transform',
+      'translateX(' + -(index * slideWidth) + '%)'
+    );
+  }
+
+  setPos(true);
+
+  function next() {
+    index++;
+    setPos();
+  }
+
+  function startAuto() {
+    timer = setInterval(next, autoDelay);
+  }
+
+  $track.on('transitionend', function () {
+    if (index >= totalSlides - visible) {
+      index = visible;
+      setPos(true);
+    }
+    if (index <= 0) {
+      index = slideCount;
+      setPos(true);
+    }
+  });
+
+  startAuto();
+});
